902d9db0bf9aaf15abae860f3e05fed1de0354f9
Sumit19jan <sumitsingh7050@gmail.com>
2021-02-02 14:05:03 +0530

    Notifications

--- app/src/main/java/com/example/shopclues_clone/MainActivity.java
+++ app/src/main/java/com/example/shopclues_clone/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.shopclues_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.shopclues_clone.activities.Notifications;
 import com.example.shopclues_clone.activities.SearchPageActivity;
 
 public class MainActivity extends AppCompatActivity {
