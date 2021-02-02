2fa6c9c604a5fb4cb10a6a8c505b5d2b125641bc
Sumit19jan <sumitsingh7050@gmail.com>
2021-02-02 11:39:36 +0530

    search page partially done

---
+++ app/src/main/java/com/example/shopclues_clone/activities/SearchPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.shopclues_clone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopclues_clone.R;
+
+public class SearchPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_page);
+    }
+}
